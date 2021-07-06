923a5dd7fe6c0210fea594943d65367012c174ec
CEREN SUSUZ <ceren199704@hotmail.com>
2021-07-06 20:09:20 +0300

    refactoring design

--- src/layouts/AdminDashboard.js
+++ src/layouts/AdminDashboard.js
@@ -1,31 +1,41 @@
 import { Grid, Menu } from 'semantic-ui-react'
-import JobSeekerList from '../pages/JobSeekerList'
-import CityList from '../pages/CityList'
-import DepartmentList from '../pages/DepartmentList'
-import JobAnnouncementList from '../pages/JobAnnouncementList'
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 
 export default function AdminDashboard() {
 
     return (
         <div>
-            <Grid className="admin" style={{ padding: '0em 1em' }}>
+            <Grid className="admin">
                 <Grid.Row>
                     <Grid.Column width={4}>
-                        <Menu pointing vertical>
-                            <Menu.Item header name='Admin' />
-                            <Menu.Item as='a' name="Job Seekers" />
-                            <Menu.Item as='a' name="Cities" />
-                            <Menu.Item as='a' name="Departments" />
-                            <Menu.Item as='a' name="Announcements" />
+                        İncele/Düzenle
+                        <Menu fluid vertical tabular>
+                            <Menu.Item header name='Yönetim Sistemleri' />
+                            <Menu.Item>
+                                <Link to={`/jobSeekerList`}>
+                                    İş Arayanlar 
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item>
+                                <Link to={`/employerList`}>
+                                    İş Verenler 
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item>
+                                <Link to={`/`}>
+                                    Yönetim Çalışanları 
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item>
+                                <Link to={`/jobAnnouncementList`}>
+                                    İş İlanları
+                                </Link>
+                            </Menu.Item>
                         </Menu>
                     </Grid.Column>
-                    <Grid.Column width={12}>
-                        <JobSeekerList />
-                        <CityList/>
-                        <DepartmentList/>
-                        <JobAnnouncementList/>
+                    <Grid.Column stretched width={12} style={{ marginTop: '6em' }}>
+
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
