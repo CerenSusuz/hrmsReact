f3fb029350efcc86ff46f9a482621130c8ad4a8a
CEREN SUSUZ <ceren199704@hotmail.com>
2021-07-09 15:26:10 +0300

    refactoring route

--- src/layouts/Dashboard.js
+++ src/layouts/Dashboard.js
@@ -12,11 +12,12 @@ import EmployerList from "../pages/EmployerList";
 import ActiveJobList from "../pages/ActiveJobList";
 import ActiveGetDeadLineJobList from "../pages/ActiveGetDeadLineJobList";
 import CityJobList from "../pages/CityJobList";
+import DepartmentJobList from "../pages/DepartmentJobList";
 import EmployerRegister from "../pages/Register/EmployerRegister";
 import JobSeekerRegister from "../pages/Register/JobSeekerRegister";
 import HrmsRegister from "../pages/Register/HrmsRegister";
 import CompanyNameJobList from "../pages/CompanyNameJobList";
-import FindJob from "../pages/FindJob";
+import AllJobList from "../pages/AllJobList";
 import Register from "../pages/Register";
 
 export default function Dashboard() {
@@ -25,21 +26,27 @@ export default function Dashboard() {
             <ToastContainer position='bottom-right' />
             <Container style={{ marginTop: '4em', paddingTop: '4em' }}>
                 <Route exact path="/" component={Homepage} />
+                {/* admin */}
                 <Route path='/admin/2a4a8a6a5a' component={AdminDashboard} />
                 <Route path="/jobSeekerList" component={JobSeekerList} />
                 <Route path="/cityList" component={CityList} />
                 <Route path="/departmentList" component={DepartmentList} />
                 <Route path="/employerList" component={EmployerList} />
                 <Route path="/jobAnnouncementList" component={JobAnnouncementList} />
+
                 <Route exact path="/activeAnnouncements" component={ActiveJobList} />
-                <Route path="/activeDeadLineAnnouncements/:deadLine" component={ActiveGetDeadLineJobList} />
-                <Route path="/cityJob" component={CityJobList} />
+                <Route path="/activeDeadLineAnnouncements/:date" component={ActiveGetDeadLineJobList} />
+                
                 <Route path="/employerRegister" component={EmployerRegister} />
                 <Route path="/jobSeekerRegister" component={JobSeekerRegister} />
                 <Route path="/register" component={Register} />
                 <Route path="/hrmsRegister" component={HrmsRegister} />
-                <Route path="/inCompanyNameJobs" component={CompanyNameJobList} />
-                <Route path="/findJob" component={FindJob} />
+
+                <Route path="/inCompanyNameJobs/:companyName" component={CompanyNameJobList} />
+                <Route path="/cityJob" component={CityJobList} />
+                <Route path="/departmentJob" component={DepartmentJobList} />
+
+                <Route path="/findJob" component={AllJobList} />
             </Container>
         </div>
     );
